885dda60d0ee6d9d0031b5f0036c32aa48206b95
Jeremy Lawler (papyrus) <jeremylawler@gmail.com>
2010-06-25 00:40:23 -0500

    adding the publisher

---
+++ publish_test.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+$:.unshift './lib'
+require 'pp'
+require 'avahi'
+def dba 
+  @avahi_dba||= Avahi::AvahiManager.new
+end
+
+def dbat
+  @avahi_dbat||=begin
+    _t = dba
+    _t.avahi_loop
+    _t
+  end
+end
+dba.publish(Avahi::IF_UNSPEC, Avahi::PROTO_UNSPEC, 'blahblaah','_hattp._tcp',35,[])
+sleep(3)
+
